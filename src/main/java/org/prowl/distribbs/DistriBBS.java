e1f3ba1c2f9338e651edcf2b9f39f013b2f2f41b
ianhawkins <ian@prowl.org>
2020-01-05 23:35:59 +0000

    Initial commit

---
+++ src/main/java/org/prowl/distribbs/DistriBBS.java
@@ -0,0 +1,14 @@
+package org.prowl.distribbs;
+
+/**
+ * DistriBBS starting class
+ * 
+ * Loads the configuration and starts the node
+ */
+public class DistriBBS
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
---
+++ src/test/java/org/prowl/distribbs/DistriBBSTest.java
@@ -0,0 +1,31 @@
+package org.prowl.distribbs;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class DistriBBSTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public DistriBBSTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( DistriBBSTest.class );
+    }
+ 
+}
