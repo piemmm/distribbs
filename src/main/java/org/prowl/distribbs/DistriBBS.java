e17364e608dd27cc979771977f772cb82b9e7316
ianhawkins <ian@prowl.org>
2020-01-07 20:15:38 +0000

    Don't quit after starting up just because we didn't start any threads

--- src/main/java/org/prowl/distribbs/DistriBBS.java
+++ src/main/java/org/prowl/distribbs/DistriBBS.java
@@ -12,27 +12,41 @@ public enum DistriBBS {
 
    INSTANCE;
 
-   private Config configuration;
+   private Config       configuration;
    private Connectivity connectivity;
 
-   DistriBBS() { }
-   
+   DistriBBS() {
+   }
+
    public void startup() {
-      
+
       // Load configuration and initialise everything needed.
       configuration = new Config();
-     
+
       // Init object storage
-      
-      
+
+
       // Init node connectors
       connectivity = new Connectivity(configuration.getConfig("connectivity"));
-      
+
       // Init node services
       connectivity.start();
-      
-      
+
+
       // All done
+      Thread t = new Thread() { 
+         public void run() {
+            while (true) {
+               try { 
+                  Thread.sleep(1000);
+
+               } catch(InterruptedException e) {
+                  e.printStackTrace();
+               }
+            }
+         }
+      };
+      t.start();
    }
 
    public static void main(String[] args) {
