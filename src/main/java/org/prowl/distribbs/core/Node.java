7b31df6b5ecb9a318c071087c03eff79ea46573d
piemmm <ian@prowl.org>
2023-02-07 18:00:20 +0000

    Mheard should know it's connector

--- src/main/java/org/prowl/distribbs/core/Node.java
+++ src/main/java/org/prowl/distribbs/core/Node.java
@@ -1,36 +1,59 @@
 package org.prowl.distribbs.core;
 
+import org.prowl.distribbs.node.connectivity.Connector;
+
 /**
  * Holder that represents another node
- * 
+ *
  * @return
  */
 public class Node {
 
-   private String callsign;
-   private long lastHeard;
-   private double rssi;
-   
-   public Node(String callsign, long lastHeard, double rssi) {
+   /**
+    * Reference to the connector the device was heard on
+    */
+   private Connector connector;
+
+   /**
+    * The node callsign
+    */
+   private String    callsign;
+
+   /**
+    * The time it was last heard
+    */
+   private long      lastHeard;
+
+   /**
+    * The signal strength (if applicable), 0 if not.
+    */
+   private double    rssi;
+
+   public Node(Connector connector, String callsign, long lastHeard, double rssi) {
       this.callsign = callsign;
       this.lastHeard = lastHeard;
       this.rssi = rssi;
+      this.connector = connector;
    }
-   
+
    public String getCallsign() {
       return callsign;
    }
-   
+
    public long getLastHeard() {
       return lastHeard;
    }
-   
+
    public void setLastHeard(long lastHeard) {
       this.lastHeard = lastHeard;
    }
-   
+
    public double getRSSI() {
       return rssi;
+   }
+
+   public Connector getConnector() {
+      return connector;
    }
 
    @Override
@@ -38,6 +61,7 @@ public class Node {
       final int prime = 31;
       int result = 1;
       result = prime * result + ((callsign == null) ? 0 : callsign.hashCode());
+      result = prime * result + ((connector == null) ? 0 : connector.hashCode());
       return result;
    }
 
@@ -55,9 +79,12 @@ public class Node {
             return false;
       } else if (!callsign.equals(other.callsign))
          return false;
+      if (connector == null) {
+         if (other.connector != null)
+            return false;
+      } else if (!connector.equals(other.connector))
+         return false;
       return true;
    }
-   
-   
-   
+
 }
